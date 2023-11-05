ed5f7e5ec3e5f7d55272bf8e365cb269856f84e0
JinCoin <149879413+Jincoin1@users.noreply.github.com>
2023-11-05 22:10:25 +0800

    Add files via upload

--- text_11.5.c
+++ text_11.5.c
@@ -1,5 +1,6 @@
 //辗转相除法；
 //求最大公约数；
+/*
 int my_division(int num1, int num2) {
     int ret = 0;
     while (num1 % num2)
@@ -25,4 +26,29 @@ int main()
     else {
         printf("最大公约数= %d\n", my_division(m, i));
     }return 0;
+}*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/*试除法求素数个数求100-200中的*/
+int main()
+{
+    int i = 0;
+    int count = 0;//素数个数
+    for (i = 101; i <= 200; i += 2)
+ {
+    int j = 0;
+    for (j = 2; j < sqrt(i); j++)
+    {
+        if (i % j == 0) {
+            break;
+        }
+    }
+    if (j > sqrt(i)) {
+        count++;
+        printf("%d ", i);
+    }
+ }
+    printf("count = %d\n",count);
+    return 0;
 }
